99b61515c091a406373e6cbc7f11d60bc7d9685b
mixednuts <jonfonts@gmail.com>
2024-12-02 22:02:07 -0600

    fix: schema corrections

--- supabase/migrations/20241130213840_skillsets.sql
+++ supabase/migrations/20241130213840_skillsets.sql
@@ -1,15 +1,16 @@
-CREATE TABLE IF NOT EXISTS "gearsets" (
+CREATE TABLE IF NOT EXISTS "skillsets" (
   "id" TEXT PRIMARY KEY NOT NULL,
   "user_id" UUID NOT NULL REFERENCES auth.users ON
   DELETE CASCADE,
   "name" TEXT NOT NULL,
-  "slots" JSONB,
   "description" TEXT,
+  "weapon" TEXT NOT NULL,
+  "tree1" JSONB,
+  "tree2" JSONB,
   "attrs" JSONB,
   "tags" JSONB,
-  "magnify" JSONB,
-  "skills" JSONB,
   "image_url" TEXT,
+  "likes" INTEGER DEFAULT 0,
   "visibility" VISIBILITY DEFAULT 'public',
   "created_at" TIMESTAMPTZ DEFAULT NOW(),
   "updated_at" timestamptz,
@@ -17,18 +18,18 @@ CREATE TABLE IF NOT EXISTS "gearsets" (
   UNIQUE("id", "user_id")
 );
 
-CREATE INDEX idx_gearsets_user_id on "gearsets"("user_id");
+CREATE INDEX idx_skillsets_user_id on "skillsets"("user_id");
 
 --------------------------------         ###  REALTIME  ###    -----------------------------------------
 -- Have to explicitly enable realtime per table
 ALTER PUBLICATION supabase_realtime
-ADD TABLE gearsets;
+ADD TABLE skillsets;
 
 ---------------------------------         ###  ROW LEVEL SECURITY  ###     -------------------------------------
 -- https://supabase.com/docs/guides/database/postgres/row-level-security
 -- https://supabase.com/docs/guides/database/postgres/row-level-security#call-functions-with-select  USE SELECT FOR PERFORMANCE
--- Enables RLS for the GEARSETS table
-ALTER TABLE gearsets ENABLE ROW LEVEL SECURITY;
+-- Enables RLS for the SKILLSETS table
+ALTER TABLE skillsets ENABLE ROW LEVEL SECURITY;
 
 -- CREATE POLICY "authenticated user listen to all" ON "realtime"."messages" AS PERMISSIVE FOR
 -- SELECT -- receive
@@ -36,8 +37,8 @@ ALTER TABLE gearsets ENABLE ROW LEVEL SECURITY;
 -- CREATE POLICY "authenticated user write to all" ON "realtime"."messages" AS permissive FOR
 -- INSERT -- send
 --   TO authenticated WITH CHECK (true);
--- Sets RLS Policy on SELECT for GEARSETS to PUBLIC. So anyone can select a GEARSETS
-CREATE POLICY "Public gearsets are viewable by everyone" ON gearsets FOR
+-- Sets RLS Policy on SELECT for SKILLSETS to PUBLIC. So anyone can select a SKILLSETS
+CREATE POLICY "Public skillsets are viewable by everyone" ON skillsets FOR
 SELECT USING (
     visibility = 'public'
     OR (
@@ -45,40 +46,40 @@ SELECT USING (
       AND (
         (
           SELECT auth.uid()
-        ) = gearsets.user_id
+        ) = skillsets.user_id
       )
     )
   );
 
 -- https://supabase.com/docs/guides/database/postgres/row-level-security#insert-policies
-CREATE POLICY "Users can insert their own gearsets." ON gearsets FOR
+CREATE POLICY "Users can insert their own skillsets." ON skillsets FOR
 INSERT TO AUTHENTICATED WITH CHECK (
     (
       SELECT auth.uid ()
-    ) = gearsets.user_id
+    ) = skillsets.user_id
   );
 
-CREATE POLICY "Users can update own gearsets." ON gearsets FOR
+CREATE POLICY "Users can update own skillsets." ON skillsets FOR
 UPDATE TO AUTHENTICATED USING (
     (
       SELECT auth.uid ()
-    ) = gearsets.user_id
+    ) = skillsets.user_id
   ) WITH CHECK (
     (
       SELECT auth.uid()
-    ) = gearsets.user_id
+    ) = skillsets.user_id
   );
 
-CREATE POLICY "Users can delete own gearsets." ON gearsets FOR DELETE TO AUTHENTICATED USING (
+CREATE POLICY "Users can delete own skillsets." ON skillsets FOR DELETE TO AUTHENTICATED USING (
   (
     SELECT auth.uid ()
-  ) = gearsets.user_id
+  ) = skillsets.user_id
 );
 
 ---------------------------------         ###  TRIGGERS  ###     -------------------------------------
 ---- Set the `updated_at` column on every update
 -- Create the trigger function
-CREATE OR REPLACE FUNCTION handle_gearsets_updated_at() RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER
+CREATE OR REPLACE FUNCTION handle_skillsets_updated_at() RETURNS TRIGGER LANGUAGE plpgsql SECURITY DEFINER
 set search_path = '' AS $$ BEGIN -- Check if the `updated_at` field is explicitly provided
   -- OR if it is unchanged
   IF NEW.updated_at IS DISTINCT
@@ -97,5 +98,5 @@ END;
 $$;
 
 -- Create the trigger
-CREATE TRIGGER handle_gearsets_updated_at BEFORE
-UPDATE ON gearsets FOR EACH ROW EXECUTE FUNCTION handle_gearsets_updated_at();
+CREATE TRIGGER handle_skillsets_updated_at BEFORE
+UPDATE ON skillsets FOR EACH ROW EXECUTE FUNCTION handle_skillsets_updated_at();
